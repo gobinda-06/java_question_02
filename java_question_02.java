fba5cc328ed3745e13decac0874748c9f43da899
Gobinda Chandra  Sahu <gobindasahu173@gmail.com>
2025-09-17 21:00:47 +0530

    Add files via upload
    A simple Java program that demonstrates how to take user input using the Scanner class and perform basic arithmetic operations. This project helps beginners understand console input/output and variable handling in Java.

---
+++ java_question_02.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class java_question_02 
+
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter First number");
+        int n =sc.nextInt();
+        System.out.println("Enter Second number");
+        int m =sc.nextInt();
+        System.out.println("sum of the number is"+(n+m));
+
+
+        
+        
+    }
+    
+}
